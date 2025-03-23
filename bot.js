26b38294c2054740d38c01687844ee54b5f78188
ThanakritGP <88748517+ThanakritGP@users.noreply.github.com>
2025-03-23 22:27:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+  res.send('Bot is Working!');
 });
 
 app.listen(port, () => {
