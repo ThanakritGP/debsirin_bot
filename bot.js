9935ac312c769834b344838c9d9e8be7cf5c9715
ThanakritGP <88748517+ThanakritGP@users.noreply.github.com>
2025-03-23 22:14:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,19 @@
+import express from 'express';
 import { Client, Events, GatewayIntentBits, REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 // Define the commands
 const commands = [
